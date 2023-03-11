a819d7ff4caa7c0237e34fac81d0c7f9f6c2e6aa
Nicolas Martyanoff <nicolas@n16f.net>
2023-03-11 16:47:54 +0100

    look for the api token in netrc

--- cmd/example/example.go
+++ cmd/example/example.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/exograd/go-program"
+	"github.com/galdor/go-netrc"
 	"github.com/galdor/go-telegram/pkg/bot"
 )
 
 func main() {
 	p := program.NewProgram("example",
 		"an example program for the go-telegram library")
+
+	p.AddArgument("username", "the telegram username of the bot")
 
 	p.SetMain(exampleMain)
 
@@ -20,8 +24,15 @@ func main() {
 }
 
 func exampleMain(p *program.Program) {
+	username := p.ArgumentValue("username")
+
+	token, err := netrcLookup("api.telegram.org", username)
+	if err != nil {
+		p.Fatal("cannot lookup token in netrc: %v", err)
+	}
+
 	botCfg := bot.BotCfg{
-		Token: "", // TODO
+		Token: token,
 	}
 
 	bot, err := bot.NewBot(botCfg)
@@ -42,4 +53,23 @@ func exampleMain(p *program.Program) {
 	case signo := <-sigChan:
 		p.Info("\nreceived signal %d (%v)", signo, signo)
 	}
+}
+
+func netrcLookup(host, login string) (string, error) {
+	var es netrc.Entries
+	if err := es.Load(netrc.DefaultPath()); err != nil {
+		return "", fmt.Errorf("cannot load netrc entries: %w", err)
+	}
+
+	s := netrc.Search{
+		Machine: host,
+		Login:   login,
+	}
+
+	matches := es.Search(s)
+	if len(matches) == 0 {
+		return "", nil
+	}
+
+	return matches[0].Password, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/galdor/go-telegram
 
 go 1.20
 
-require github.com/exograd/go-program v0.0.0-20220515082050-4ab3df8c5da5 // indirect
+require (
+	github.com/exograd/go-program v0.0.0-20220515082050-4ab3df8c5da5
+	github.com/galdor/go-netrc v0.0.0-20230311153918-ac75a28f9099
+)
